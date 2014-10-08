0a769e25e296f287e7675fa74af310eb75617aaa
Sajal Kayan <sajal@turbobytes.com>
2014-10-08 16:26:54 +0700

    hardcode our settings.py

--- conf/gen.settings.sh
+++ conf/gen.settings.sh
@@ -1,30 +1,15 @@
 #!/bin/sh
 
 cat << EOF
-# --
-# -- Disco settings
-# --
-# The defaults should be pretty sane, so be careful changing them.
 
-# Home of the Disco libraries
-DISCO_HOME = "${ABSTARGETLIB}"
-
-# Root directory for Disco data
-DISCO_ROOT = "${ABSTARGETSRV}"
-
-# Where the master's web docroot lives
-DISCO_WWW_ROOT = "${ABSTARGETDAT}/${WWW}"
-
-# HTTP server for master and nodes runs on this port
-# disco://host URIs are mapped to http://host:DISCO_PORT
+DISCO_HOME = "/usr/lib/disco"
+DISCO_ROOT = "/mnt/disco"
+DISCO_WWW_ROOT = "/usr/share/disco/master/www"
 DISCO_PORT = 8989
-
-# Example config for Varnish proxy
-# DISCO_PROXY_ENABLED = "on"
-# DISCO_HTTPD = "/usr/sbin/varnishd -a 0.0.0.0:\$DISCO_PROXY_PORT -f \$DISCO_PROXY_CONFIG -P \$DISCO_PROXY_PID -n/tmp -smalloc"
-
+DISCO_PROXY_ENABLED = "on"
 DDFS_TAG_MIN_REPLICAS = 3
 DDFS_TAG_REPLICAS     = 3
 DDFS_BLOB_REPLICAS    = 3
+DISCO_GC_AFTER = 1800
 
 EOF
